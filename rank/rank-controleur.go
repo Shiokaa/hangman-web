f5184f769905e67c8b9d42db4f01c11c21c35eb3
Amaru Tom <tom.amaru@ynov.com>
2024-10-29 09:32:16 +0100

    rank

--- rank/rank-controleur.go
+++ rank/rank-controleur.go
@@ -3,13 +3,31 @@ package rank
 import (
 	"HangmanWeb/home"
 	"HangmanWeb/templates"
+	"html/template"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func rank(w http.ResponseWriter, r *http.Request) {
-	file, _ := os.ReadFile("./rank/rank.txt")
-	os.WriteFile("./rank/rank.txt", []byte(string(file)+"Joueur : "+home.Pseudo+" Difficulté : "+home.Difficulty+"\n"), 0666)
 
-	templates.Templates.ExecuteTemplate(w, "rank", nil)
+	if home.Pseudo == "" {
+		http.Redirect(w, r, "/erreur?code=400&message=Oups méthode incorrecte", http.StatusMovedPermanently)
+		return
+	}
+
+	// Lire le contenu du fichier existant
+	file, _ := os.ReadFile("./rank/rank.txt")
+
+	// Écrire les nouvelles données dans le fichier
+	os.WriteFile("./rank/rank.txt", []byte(string(file)+"Joueur : "+home.Pseudo+" Difficulte : "+home.Difficulty+"\n"), 0666)
+
+	// Lire à nouveau le fichier mis à jour
+	rank, _ := os.ReadFile("./rank/rank.txt")
+
+	// Remplacer les nouvelles lignes par <br> pour HTML et convertir en template.HTML
+	rankHTML := template.HTML(strings.ReplaceAll(string(rank), "\n", "<br>"))
+
+	// Passer le contenu formaté au template
+	templates.Templates.ExecuteTemplate(w, "rank", rankHTML)
 }
